8870610d9b229c31a5c3315793c8283a7a493909
Agustín Moragues Beckar <moraguesbeckar91@gmail.com>
2020-11-29 22:57:04 -0300

    Adding mongoose connection and nodemon

---
+++ config/config.js
@@ -0,0 +1,6 @@
+var myConfig = {
+    url: 'mongodb://localhost:27017',
+    bd:'vicentiwebapp'
+ }
+ 
+ module.exports = myConfig;
--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 'use strict'
 
+const mongoose=require('mongoose');
+const config = require('./config/config');
+
+
+mongoose.set('useFindAndModify',false);
+mongoose.Promise=global.Promise;
+
+
+mongoose.connect(config.url + config.bd,{useNewUrlParser:true}).then(
+    ()=>{
+        console.log("Conexión a BD,perfecta y correcta");
+        console.log(config.url);
+    }
+);
+
